3ff0270b491cc9d4e606e9973e0eac45b03e6bba
Claudio Rossi <claudio@eurosoftlab.com>
2019-03-02 11:41:48 +0100

    project creation

---
+++ convertCSV2XML.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class convertCSV2XML
+	Inherits System.Windows.Forms.Form
+
+	'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Richiesto da Progettazione Windows Form
+	Private components As System.ComponentModel.IContainer
+
+	'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+	'Può essere modificata in Progettazione Windows Form.  
+	'Non modificarla mediante l'editor del codice.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'OpenFileDialog
+		'
+		Me.OpenFileDialog.FileName = "OpenFileDialog"
+		Me.OpenFileDialog.Filter = "csv files|*.csv"
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(116, 70)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 23)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "Button1"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'convertCSV2XML
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.Button1)
+		Me.Name = "convertCSV2XML"
+		Me.Text = "Form1"
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents OpenFileDialog As OpenFileDialog
+	Friend WithEvents Button1 As Button
+End Class
---
+++ convertCSV2XML.vb
@@ -0,0 +1,184 @@
+﻿
+#Region " Tipi "
+
+''' <summary>
+''' separatore da usare per i file CSV
+''' </summary>
+''' <remarks></remarks>
+Public Enum enumSeparatore
+	pipe = 1
+	semicolon = 2
+	comma = 3
+	tab = 4
+End Enum
+
+#End Region
+
+Public Class convertCSV2XML
+
+#Region " Gestori di evento "
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Me.Openfile()
+	End Sub
+
+#End Region
+
+#Region " Metodi privati "
+
+	''' <summary>
+	''' Select CSV file
+	''' </summary>
+	Private Sub Openfile()
+		Dim result As DialogResult
+		Dim filename As String
+
+		result = Me.OpenFileDialog.ShowDialog()
+
+		If result = DialogResult.OK Then
+			'user confirmed selection
+			filename = Me.OpenFileDialog.FileName
+
+			If filename <> "" Then
+				'user selected a file
+
+				Me.ConvertData(filename)
+			End If
+		End If
+	End Sub
+
+	''' <summary>
+	''' Convert data from CSV format to XML
+	''' </summary>
+	''' <param name="filename">name of the file to import</param>
+	Private Sub ConvertData(filename As String)
+		Dim tmpDT As DataTable
+		Dim i As Integer
+		Dim subject As String
+		Dim serializer As Xml.Serialization.XmlSerializer
+		Dim xmlDocument As New Xml.XmlDocument
+		Dim xmlDeclaration As Xml.XmlDeclaration
+		Dim root, element As Xml.XmlElement
+
+		tmpDT = Me.ImportCSV(filename, enumSeparatore.semicolon, True)
+
+		If tmpDT IsNot Nothing Then
+			subject = ""
+
+			'init XML structure
+			xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", "")
+			root = xmlDocument.DocumentElement
+			xmlDocument.InsertBefore(xmlDeclaration, root)
+
+			element = xmlDocument.CreateElement(String.Empty, "body", String.Empty)
+			xmlDocument.AppendChild(element)
+
+			For i = 0 To tmpDT.Rows.Count - 1
+				'for esch row in table 
+				If subject = tmpDT.Rows(i).Item("subject_name").ToString Then
+					'same subject of previous rows
+				Else
+					'subject has changed
+
+				End If
+			Next
+		End If
+	End Sub
+
+	''' <summary>
+	''' read a CSV file and put the content into a datatable
+	''' </summary>
+	''' <param name="fileName">name of the file to import</param>
+	''' <param name="separator">separator</param>
+	''' <returns>tabella contenente i dati importati</returns>
+	''' <remarks></remarks>
+	Private Function ImportCSV(ByVal fileName As String, ByVal separator As enumSeparatore, ByVal dropEmptyRows As Boolean) As DataTable
+		Dim myFile As IO.StreamReader
+		Dim fileContent, sep, columnName As String
+		Dim result As New DataTable
+		Dim row As String()
+
+		sep = ""
+		Select Case separator
+			Case enumSeparatore.pipe
+				sep = "|"
+			Case enumSeparatore.semicolon
+				sep = ";"
+			Case enumSeparatore.comma
+				sep = ","
+		End Select
+
+		Try
+			myFile = New IO.StreamReader(fileName)
+			fileContent = myFile.ReadLine
+
+			row = fileContent.Split(sep(0))
+
+			For Each columnName In row
+				'removes unnecessary "
+				columnName = columnName.Replace("""", "")
+
+				'add column to table structure
+				result.Columns.Add(columnName)
+			Next
+
+			Do
+				fileContent = myFile.ReadLine
+				If dropEmptyRows Then
+					While fileContent IsNot Nothing AndAlso fileContent = ""
+						fileContent = myFile.ReadLine
+					End While
+				End If
+				If fileContent IsNot Nothing AndAlso fileContent <> "" Then
+
+					fileContent = RemoveHTMLEscapeSequences(fileContent)
+
+					row = fileContent.Split(sep(0))
+					result.Rows.Add(row)
+				End If
+			Loop While fileContent <> Nothing
+
+		Catch ex As IO.IOException
+			Throw New Exception("error accessing file" & fileName)
+		End Try
+
+		Return result
+	End Function
+
+	''' <summary>
+	''' Replace HTML escape sequences to avoid problems with csv separator
+	''' </summary>
+	''' <param name="value">string to clean</param>
+	''' <returns></returns>
+	Private Function RemoveHTMLEscapeSequences(value As String) As String
+		Dim result As String
+
+		result = value
+
+		result = result.Replace("&quot;", "'")
+		result = result.Replace("&rsquo;", "'")
+		result = result.Replace("&lsquo;", "'")
+		result = result.Replace("&tilde;", "~")
+		result = result.Replace("&agrave;", "à")
+		result = result.Replace("&egrave;", "è")
+		result = result.Replace("&Egrave;", "È")
+		result = result.Replace("&eacute;", "é")
+		result = result.Replace("&ograve;", "ò")
+		result = result.Replace("&ugrave;", "ù")
+		result = result.Replace("&igrave;", "ì")
+		result = result.Replace("&amp;", "&")
+		result = result.Replace("&larr;", "<--")
+		result = result.Replace("&times;", "x")
+		result = result.Replace("&gt;", ">")
+		result = result.Replace("&lt;", "<")
+		result = result.Replace("&pi;", "pi greco")
+		result = result.Replace("&frac12;", "1/2")
+		result = result.Replace("&frac14;", "1/4")
+		result = result.Replace("&frac34;", "3/4")
+
+		Return result
+	End Function
+
+#End Region
+
+End Class
